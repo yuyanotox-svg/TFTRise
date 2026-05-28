6b87ee1a28f1384024e4667a704fb93ec5539f66
Yuzuri <yuya.noto.x@gmail.com>
2026-05-28 11:48:47 +0900

    Normalize Supabase project URL

--- api/config.js
+++ api/config.js
@@ -1,7 +1,8 @@
 module.exports = function handler(req, res) {
+  const supabaseUrl = (process.env.VITE_SUPABASE_URL || "").replace(/\/rest\/v1\/?$/, "");
   res.setHeader("Cache-Control", "no-store");
   res.status(200).json({
-    supabaseUrl: process.env.VITE_SUPABASE_URL || "",
+    supabaseUrl,
     supabaseAnonKey: process.env.VITE_SUPABASE_ANON_KEY || "",
   });
 };
